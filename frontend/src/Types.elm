708e06edf97e1f115211724d521c13f60f7ded56
Matthieu Pizenberg <matthieu.pizenberg@gmail.com>
2025-05-26 17:14:55 +0200

    Add other ...ToData conversion functions

--- frontend/src/Types.elm
+++ frontend/src/Types.elm
@@ -4,7 +4,7 @@ import Bytes.Comparable as Bytes exposing (Bytes)
 import Cardano.Address exposing (CredentialHash)
 import Cardano.Data as Data exposing (Data)
 import Cardano.MultiAsset exposing (AssetName, PolicyId)
-import Cardano.Value exposing (Value)
+import Cardano.Value as Value exposing (Value)
 import Integer
 import MultisigScript exposing (MultisigScript)
 import Natural as N exposing (Natural)
@@ -82,10 +82,10 @@ treasurySpendRedeemerToData redeemer =
             Data.Constr N.one []
 
         Fund amount ->
-            Data.Constr N.two [ Cardano.Value.toData amount ]
+            Data.Constr N.two [ Value.toData amount ]
 
         Disburse amount ->
-            Data.Constr N.three [ Cardano.Value.toData amount ]
+            Data.Constr N.three [ Value.toData amount ]
 
 
 
@@ -93,10 +93,19 @@ treasurySpendRedeemerToData redeemer =
 
 
 type alias VendorConfiguration =
-    { registryToken : PolicyId
+    { registryToken : Bytes PolicyId
     , permissions : VendorPermissions
-    , expiration : Int
+    , expiration : Natural
     }
+
+
+vendorConfigToData : VendorConfiguration -> Data
+vendorConfigToData { registryToken, permissions, expiration } =
+    Data.Constr N.zero
+        [ Data.Bytes <| Bytes.toAny registryToken
+        , vendorPermissionsToData permissions
+        , Data.Int <| Integer.fromNatural expiration
+        ]
 
 
 type alias VendorPermissions =
@@ -106,22 +115,58 @@ type alias VendorPermissions =
     }
 
 
+vendorPermissionsToData : VendorPermissions -> Data
+vendorPermissionsToData { pause, resume, modify } =
+    Data.Constr N.zero
+        [ MultisigScript.toData pause
+        , MultisigScript.toData resume
+        , MultisigScript.toData modify
+        ]
+
+
 type alias VendorDatum =
     { vendor : MultisigScript
     , payouts : List Payout
     }
 
 
+vendorDatumToData : VendorDatum -> Data
+vendorDatumToData { vendor, payouts } =
+    Data.Constr N.zero
+        [ MultisigScript.toData vendor
+        , Data.List <| List.map payoutToData payouts
+        ]
+
+
 type alias Payout =
-    { maturation : Int
+    { maturation : Natural
     , value : Value
     , status : PayoutStatus
     }
 
 
+payoutToData : Payout -> Data
+payoutToData { maturation, value, status } =
+    Data.Constr N.zero
+        [ Data.Int <| Integer.fromNatural maturation
+        , Value.toData value
+        , payoutStatusToData status
+        ]
+
+
 type PayoutStatus
     = Active
     | Paused
+
+
+payoutStatusToData : PayoutStatus -> Data
+payoutStatusToData status =
+    case status of
+        Active ->
+            Data.Constr N.zero []
+
+        Paused ->
+            Data.Constr N.one []
 
 
 type VendorSpendRedeemer
@@ -130,3 +175,22 @@ type VendorSpendRedeemer
     | Modify
     | SweepVendor
     | Malformed
+
+
+vendorSpendRedeemerToData : VendorSpendRedeemer -> Data
+vendorSpendRedeemerToData redeemer =
+    case redeemer of
+        Withdraw ->
+            Data.Constr N.zero []
+
+        Adjudicate statuses ->
+            Data.Constr N.one [ Data.List <| List.map payoutStatusToData statuses ]
+
+        Modify ->
+            Data.Constr N.two []
+
+        SweepVendor ->
+            Data.Constr N.three []
+
+        Malformed ->
+            Data.Constr N.four []
